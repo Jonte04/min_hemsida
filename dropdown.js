83b004797f588d231927b6ed9a85db5f20606db4
Jonatan Särnhammar <jonatansarnhammar@gmail.com>
2021-05-25 12:18:46 +0200

    Toggable drop-down menu

--- dropdown.js
+++ dropdown.js
@@ -1,11 +1,11 @@
-document
-  .getElementById("drop-down-menu")
-  .addEventListener("click", function () {
-    var drowDownMenu = document.getElementById("drop-down-menu");
-
-    if (drowDownMenu.style.display === "none") {
-      drowDownMenu.style.display = "flex";
-    } else {
-      drowDownMenu.style.display = "none";
-    }
-  });
+let toggle;
+console.log(toggle);
+function show_hide() {
+  if (toggle == 1) {
+    document.getElementById("drop-down-menu").style.display = "none";
+    return (toggle = 0);
+  } else {
+    document.getElementById("drop-down-menu").style.display = "flex";
+    return (toggle = 1);
+  }
+}
